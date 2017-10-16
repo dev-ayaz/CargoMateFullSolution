51cee1f538a15f17c893147bc2a787d1de826b32
AyazTanzil <mtanzil666@gmail.com>
2017-10-17 00:00:38 +0300

    Tanzil Commit 17-10-2017

---
+++ CargoMate.Web.FrontEnd/AppScripts/Home/Home.js
@@ -0,0 +1,22 @@
+﻿var Home = {
+    Selectors: {
+
+        AddressPicker: ".placePicker",
+    },
+    Callbacks: {
+
+    },
+    InitEvents: function(){
+
+        $(Home.Selectors.AddressPicker).placepicker({
+            placeChanged: function (place) {
+                var location = this.getLocation();
+                console.log(location);
+            }
+        }).data("placepicker");
+    }
+};
+
+jQuery(function () {
+    Home.InitEvents();
+});
--- CargoMate.Web.FrontEnd/Controllers/HomeController.cs
+++ CargoMate.Web.FrontEnd/Controllers/HomeController.cs
@@ -47,5 +47,9 @@ namespace CargoMate.Web.FrontEnd.Controllers
         public ActionResult VehicleDetails() {
             return View();
         }
+
+        public ActionResult SearchResults() {
+            return View();
+        }
     }
 }
